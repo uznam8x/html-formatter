55d7344e19d5fb3ee76586126bda4e800272ffac
jinam Yu <znu@thenet-global.com>
2018-03-12 15:53:22 +0900

    update

--- src/minify.js
+++ src/minify.js
@@ -1,23 +1,22 @@
 var minify = function(el)  {
-    return el
-        .replace(/\n|\t/g, '')
-        .replace(/[a-z]+="\s*"/ig, '')
-        .replace(/>\s+</g, '><')
-        .replace(/\s+/g, ' ')
-        .replace(/"\s>/g, '">')
-        //.replace(/"\s/g, '"')
-        .replace(/>\s/g, '>')
-        .replace(/\s</g, '<')
-        .replace(/class=["']\s/g, function(match) {
-            return match.replace(/\s/g, '');
-        })
-        ;
+  return el
+    .replace(/\n|\t/g, '')
+    .replace(/[a-z]+="\s*"/ig, '')
+    .replace(/>\s+</g, '><')
+    .replace(/\s+/g, ' ')
+    .replace(/\s>/g, '>')
+    .replace(/>\s/g, '>')
+    .replace(/\s</g, '<')
+    .replace(/class=["']\s/g, function(match) {
+      return match.replace(/\s/g, '');
+    })
+    ;
 };
 
 if (typeof define === "function" && define.amd) {
-    define([], function() {
-        return minify;
-    });
+  define([], function() {
+    return minify;
+  });
 } else if (typeof module !== "undefined") {
-    module.exports = minify;
+  module.exports = minify;
 }
--- src/render.js
+++ src/render.js
@@ -1,7 +1,6 @@
-
-var closing = require(__dirname+'/closing.js');
-var entity = require(__dirname+'/entity.js');
-var minify = require(__dirname+'/minify.js');
+var closing = require(__dirname + '/closing.js');
+var entity = require(__dirname + '/entity.js');
+var minify = require(__dirname + '/minify.js');
 
 var option = {};
 
@@ -10,32 +9,32 @@ var convert = {
   line: []
 };
 
-var comment = function(el){
+var comment = function (el) {
   convert.comment = [];
-  el = el.replace(/(<!--[^>]*?>)/g, function(match) {
+  el = el.replace(/(<!--[^>]*?>)/g, function (match) {
     convert.comment.push(match);
     return '<!-- [#!# : ' + (convert.comment.length - 1) + ' : #!#] -->';
   });
   return el
 };
 
-var line = function(el){
+var line = function (el) {
   convert.line = [];
   var i = -1;
-  el = el.replace(/<[^>]*>/g, function(match) {
+  el = el.replace(/<[^>]*>/g, function (match) {
     convert.line.push(match);
     i++;
-    return '\n[#-# : '+i+' : '+match+' : #-#]\n';
+    return '\n[#-# : ' + i + ' : ' + match + ' : #-#]\n';
   });
   el = el.replace(/\n\n/g, '\n');
   return el;
 };
 
-var tidy = function(el){
+var tidy = function (el) {
   var tab = '';
-  convert.line.forEach(function(source, index) {
-    el = el.replace('[#-# : '+index+' : '+source+' : #-#]', function(match) {
-      var prevLine = '[#-# : '+(index - 1)+' : '+convert.line[index - 1]+' : #-#]';
+  convert.line.forEach(function (source, index) {
+    el = el.replace('[#-# : ' + index + ' : ' + source + ' : #-#]', function (match) {
+      var prevLine = '[#-# : ' + (index - 1) + ' : ' + convert.line[index - 1] + ' : #-#]';
       tab += '\t';
       var remove = 0;
       if (index === 0) remove++;
@@ -53,21 +52,17 @@ var tidy = function(el){
   });
 
   el = el
-    .replace(/\n<!doctype/g, '<!doctype')
     .replace(/>\n([\w+!@#$%^&*\(\)_+\-=\?\:;"'\/\.,\[\]\{\}\|]+)/g, '>$1')
     .replace(/([\w+!@#$%^&*\(\)_+\-=\?\:;"'\/\.,\[\]\{\}\|])\n\s+<\//g, '$1</')
-    .replace(/<[^\/]([^>]*?)>\s+<\//g, function(match, capture){
-      if( match.indexOf('<img') > -1 || match.indexOf('<input') > -1 ){
+    .replace(/<[^\/]([^>]*?)>\s+<\//g, function (match, capture) {
+      if (match.indexOf('/>') > -1) {
         return match;
       }
-      return match.replace(/\s+/g, '');
-    })
-    .replace(/<(\w+)[^>]*[^\/]>\n\s+<\/(\w+)>/g, function(match) {
-      return match.replace(/\n|\t/g, '').replace(/\s+/g, ' ');
+      return match.replace(/\s\s+/g, '');
     })
 
-  convert.comment.forEach(function(source, index) {
-    el = el.replace(new RegExp('<!--[^>]*' + index + ' : #!#] -->', 'g'), function(match) {
+  convert.comment.forEach(function (source, index) {
+    el = el.replace(new RegExp('<!--[^>]*' + index + ' : #!#] -->', 'g'), function (match) {
       var cnt = /%(\d+)%/g.exec(match);
       var t = '';
       for (var c = 0; c < cnt[1]; c++) {
@@ -76,10 +71,10 @@ var tidy = function(el){
       return source.replace(/\n/g, '\n' + t);
     });
   });
-  return el.substring(1, el.length-1);
+  return el.substring(1, el.length - 1);
 };
 
-var render = function(el, opt) {
+var render = function (el, opt) {
   el = closing(el);
   el = comment(el);
   el = entity(el);
@@ -92,7 +87,7 @@ var render = function(el, opt) {
 };
 
 if (typeof define === "function" && define.amd) {
-  define([], function() {
+  define([], function () {
     return render;
   });
 } else if (typeof module !== "undefined") {
